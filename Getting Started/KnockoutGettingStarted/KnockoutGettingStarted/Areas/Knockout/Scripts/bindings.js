e041912a81e58da342dd45ae08b81840136b4c58
ksmykala <ksmykala@future-processing.com>
2015-01-05 13:39:09 +0100

    Bootstrap has been added, simple jobs binding has been finished.

--- Getting Started/KnockoutGettingStarted/KnockoutGettingStarted/Areas/Knockout/Scripts/bindings.js
+++ Getting Started/KnockoutGettingStarted/KnockoutGettingStarted/Areas/Knockout/Scripts/bindings.js
@@ -1,8 +1,14 @@
 ﻿"use strike";
 
-var Company = function(index, name) {
+var Company = function(index, name, jobs) {
     this.index = index;
     this.name = name;
+    this.jobs = jobs;
+}
+
+var Job = function(name, salary) {
+    this.name = name;
+    this.salary = salary;
 }
 
 var BindingsViewModel = function () {
@@ -11,34 +17,17 @@ var BindingsViewModel = function () {
     self.plainText = "Plain Text";
 
     self.availableCompanies = ko.observableArray([
-        new Company(0, "Future Processing"),
-        new Company(1, "Sii"),
-        new Company(2, "BI-Technologies")
+        new Company(0, "Google", [new Job("QA", 1999.99), new Job("Developer", 2000), new Job("CEO", 1.99^10)]),
+        new Company(1, "Yahoo", [new Job("Architect", 2000), new Job("PM", 10124)]),
+        new Company(2, "NoNameCompany", [new Job("Tester", 666.666), new Job("Awesome driver", 2500), new Job("Nobody", 9999)])
     ]);
 
     self.selectedCompany = ko.observable(0);
 
+    self.getJobsInSelectedCompany = function() {
+        return self.availableCompanies()[self.selectedCompany()].jobs;
+    };
 }
-
-ko.bindingHandlers.toggleWizardSection = {
-    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-        var value = valueAccessor();
-        var valueUnwrapped = ko.unwrap(value);
-
-        if (valueUnwrapped === undefined)
-            $(element).css('display', 'none');
-    },
-    update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-        var value = valueAccessor();
-        var valueUnwrapped = ko.unwrap(value);
-        var duration = allBindings.get('duration') || 400;
-
-        if(valueUnwrapped === undefined)
-            $(element).hide(duration);
-        else
-            $(element).show(duration);
-    }
-};
 
 var viewModel = new BindingsViewModel();
 

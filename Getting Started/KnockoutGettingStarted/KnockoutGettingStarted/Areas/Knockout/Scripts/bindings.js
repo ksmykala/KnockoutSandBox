080e06b02360411019134978481c7d76585138e1
ksmykala <ksmykala@future-processing.com>
2015-01-05 11:51:18 +0100

    Simple binding with select tag and plain text.

---
+++ Getting Started/KnockoutGettingStarted/KnockoutGettingStarted/Areas/Knockout/Scripts/bindings.js
@@ -0,0 +1,26 @@
+﻿"use strike";
+
+var Company = function(index, name) {
+    this.index = index;
+    this.name = name;
+}
+
+var BindingsViewModel = function () {
+    var self = this;
+
+    self.plainText = "Plain Text";
+
+    self.companies = {
+        available: ko.observableArray([
+            new Company(0, "Future Processing"),
+            new Company(1, "Sii"),
+            new Company(2, "BI-Technologies")
+        ]),
+        selected: ko.observable()
+    };
+
+}
+
+var viewModel = new BindingsViewModel();
+
+ko.applyBindings(viewModel);
